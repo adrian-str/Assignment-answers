ffa34d2bc4e54aab4146ad665916f6c02b2a154a
adrian-str <adrian-str@github.com>
2020-11-02 09:43:25 -0500

    hard

--- Assignment_2/InteractionNetwork.rb
+++ Assignment_2/InteractionNetwork.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 class InteractionNetwork
   
   attr_accessor :network
@@ -11,35 +13,60 @@ class InteractionNetwork
     @members = params.fetch(:members,'NA')
     @kegg_path = params.fetch(:kegg_path,'NA')
     @go_terms = params.fetch(:go_terms,'NA')
+  end
   
-end
-
-def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
-  response = RestClient::Request.execute({
-    method: :get,
-    url: url.to_s,
-    user: user,
-    password: pass,
-    headers: headers})
-  return response
-  
-  rescue RestClient::ExceptionWithResponse => e
-    $stderr.puts e.inspect
-    response = false
-    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
-  rescue RestClient::Exception => e
-    $stderr.puts e.inspect
-    response = false
-    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
-  rescue Exception => e
-    $stderr.puts e.inspect
-    response = false
-    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
-end 
+  def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
+    response = RestClient::Request.execute({
+      method: :get,
+      url: url.to_s,
+      user: user,
+      password: pass,
+      headers: headers})
+    return response
+    
+    rescue RestClient::ExceptionWithResponse => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+    rescue RestClient::Exception => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+    rescue Exception => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  end
+   
   
   def self.search_interactors(file)
-    File.open(file).each do |code|
-      res=fetch('http://bar.utoronto.ca:9090/psicquic/webservices/current/search/#{code.strip}/?format=tab25')
-      
+        @intact=Hash.new
+        File.open(file).each do |code|
+          
+          res = fetch("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/query/#{code.strip}%20AND%20species:arath/?format=tab25")
+          @intact[code.strip]=res.body.split("\n")
+           
     end
+    
+  end
+  
+  def self.search_ppi(intact, cutv=0.485)
+    @genes=intact.keys
+    
+    score=/i\w+-\w+:(0\.\d+)/
+    intact.each do |a,int|
+      int.each do |i|
+        g1,g2=i.scan(/A[T]\d[G]\d\d\d\d\d/)
+        score=i.match(score)[1].to_f
+        next if score<=cutv
+        if @genes===g1||@genes===g2
+          
+            #code
+        end
+        
+        
+      end
+    end
+    end
+  end
 end
