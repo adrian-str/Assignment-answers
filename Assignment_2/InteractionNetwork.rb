789b56f498b64832966aa86609481577b3d75728
adrian-str <adrian-str@github.com>
2020-11-07 04:10:27 -0500

    infinite loops

--- Assignment_2/InteractionNetwork.rb
+++ Assignment_2/InteractionNetwork.rb
@@ -42,37 +42,79 @@ class InteractionNetwork
   def self.get_agi(file)
     @@genes=[]
     File.open(file).each do |code|
-      @@genes << code
+      @@genes << code.strip
     end
-    return @@genes
+    
   end
   
-  @counter=0
-  def self.search_interactors(genes)
-    @counter=+1
-    @interacts=Hash.new    
-    if genes.is_a?(Array)
-      genes.each do |code|
-        res = fetch("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{code}%20AND%20species:arath/?format=tab25")
-        score=/i\w+-\w+:(0\.\d+)/
-        if res
-          intact=res.body.split("\n")
-          intact.each do |int|
-            g1=int.match(/(A[T]\d[G]\d\d\d\d\d)\(locus\sname\)/)[1]
-            score=int.match(score)[1].to_f
-            next if score<=cutv
-            if @@genes===g1
-              @interacts[code]=g1
-            else
-              search_interactors(g1)
+  def self.get_interacting
+    return @@interacting
+  end
+  
+  def self.search_interactors
+    @@counter=0
+    @@interacting=Hash.new
+      
+    @@genes.each do |code|
+      
+      InteractionNetwork.get_interactors(code)
+      
+    end
+    
+  end
+  
+  def self.get_interactors(code,cutv=0.485)
+    
+      
+    
+      @@counter=+1
+      
+       
+      
+      res = fetch("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{code}%20AND%20species:arath/?format=tab25")
+      miscore=/i\w+-\w+:(0\.\d+)/
+      if res
+        intact=res.body.split("\n")
+        intact.each do |int|
+          puts int
+          
+          next if int.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/).nil?
+             
+            g1,g2=int.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/)
+            g1=g1[0]
+            g2=g2[0]
+            puts g2
+          next if g1.nil?||g2.nil?
+          if g1.downcase==code.downcase #get the interactor not the same gene
+            
+            g1=g2
+            
+          end
+          
+          score=int.match(miscore)[1].to_f
+          
+          next if score<cutv
+          if @@genes.any?{|i| i.downcase==g1.downcase}
+            @@interacting[code]=g1
+            puts "INTERACTTING:#{@@interacting}"
+            
+          else
+            puts "these were not"
+            puts g1
+            if @@counter<2
               
+              InteractionNetwork.get_interactors(g1)
+            else
+              next
             end
           end
+            
+            
         end
       end
-    end 
-  end
-  
+    
+    
+  end  
 
   
 
