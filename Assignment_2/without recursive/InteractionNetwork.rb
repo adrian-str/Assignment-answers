6397ca44d7d3393a1d4614ce1174ca16edd1d5c8
adrian-str <adrian-str@github.com>
2020-11-07 13:13:28 -0500

    no recursive

---
+++ Assignment_2/without recursive/InteractionNetwork.rb
@@ -0,0 +1,185 @@
+require 'rest-client'
+require 'json'
+class InteractionNetwork
+  
+  attr_accessor :network
+  attr_accessor :members
+  attr_accessor :kegg_path
+  attr_accessor :go_terms
+  @@all_interactions=[]
+  
+  def initialize(params={})
+    @network = params.fetch(:network,0)
+    @members = params.fetch(:members,'NA')
+    @kegg_path = annotate_kegg(ids=@members)
+    @go_terms = annotate_GO(ids=@members)
+    @@all_interactions << self
+  end
+  
+  def self.fetch(url, headers = {accept: "*/*"}, user = "", pass="")
+    response = RestClient::Request.execute({
+      method: :get,
+      url: url.to_s,
+      user: user,
+      password: pass,
+      headers: headers})
+    return response
+    
+    rescue RestClient::ExceptionWithResponse => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+    rescue RestClient::Exception => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+    rescue Exception => e
+      $stderr.puts e.inspect
+      response = false
+      return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  end
+   
+  def self.get_agi(file)
+    @@genes=[]
+    File.open(file).each do |code|
+      @@genes << code.strip.downcase
+    end
+    return @@genes
+  end
+  
+  def self.get_interacting
+    return @@interacting
+  end
+  
+  def self.search_interactors
+    @@counter=0
+    @@interacting=Hash.new
+      
+    @@genes.each do |code|
+      
+      InteractionNetwork.get_interactors(code)
+      
+    end
+    
+  end
+  
+  def self.get_interactors(code,cutv=0.485)
+      genes=@@genes
+      res = InteractionNetwork.fetch("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{code}%20AND%20species:arath/?format=tab25")
+      miscore=/i\w+-\w+:(0\.\d+)/
+      if res
+        intact=res.body.split("\n")
+        intact.each do |int|
+          
+          next if int.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/).nil?
+             
+            g1,g2=int.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/)
+          next if g1.nil?||g2.nil?
+            g1=g1[0]
+            g2=g2[0]
+            
+          
+          if g1.downcase==code.downcase #get the interactor not the same gene
+            
+            g1=g2
+            
+          end
+          
+          score=int.match(miscore)[1].to_f
+          
+          next if score<cutv
+          if genes.include?(g1.downcase)
+             @@interacting[code]=g1
+             
+            
+          else
+            g0=g1
+            res = InteractionNetwork.fetch("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{g0}%20AND%20species:arath/?format=tab25")
+            if res
+              intact2=res.body.split("\n")
+              intact2.each do |int2|
+          
+                next if int2.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/).nil?
+               
+                g1,g2=int2.scan(/(A[Tt]\d[Gg]\d\d\d\d\d)\(locus\sname\)/)
+                next if g1.nil?||g2.nil?
+                g1=g1[0]
+                g2=g2[0]
+              
+                
+                if g1.downcase==g0.downcase #get the interactor not the same gene
+              
+                  g1=g2
+              
+                end
+                next if g1.downcase==code.downcase
+                score=int.match(miscore)[1].to_f
+            
+                next if score<cutv
+                if genes.include?(g1.downcase)
+                  @@interacting[code]=[g0,g1]
+                end
+              end
+            
+            
+            end
+          end  
+            
+        end
+      end
+    
+    
+  end  
+
+  def annotate_kegg(ids,db="genes",field="pathways")
+    
+    ids.each do |id|
+    
+      res = InteractionNetwork.fetch("http://togows.org/entry/#{db}/#{id}/#{field}.json")
+      if res
+        annotation=JSON.parse(res.body)[0]
+        return annotation
+      end
+      
+    end    
+        
+  end    
+  
+  def annotate_GO(ids,db="uniprot",field="dr")
+    
+    ids.each do |id|
+    
+      resp = InteractionNetwork.fetch("http://togows.org/entry/#{db}/#{id}/#{field}.json")
+      annotations=[]
+      if resp
+        res=JSON.parse(resp.body)[0]
+        res["GO"].each do |term|
+          if term[1]=~/P:/ #P=biological processes
+            goid=term[0]
+            goterm=term[1].match(/:(.+)/)[1]
+            annotation=[goid,goterm]
+            annotations << annotation
+            
+          end
+        end
+        return annotations.uniq
+      end
+      
+    end    
+        
+  end
+  
+  def self.load
+    count=0
+    ids=[]
+    @@interacting.each do |key,value|
+      count+=1
+      ids=[key,value]
+      InteractionNetwork.new(:network => count, :members =>ids)
+    end
+  end
+  
+  def self.get_all
+    return @@all_interactions
+  end
+end
