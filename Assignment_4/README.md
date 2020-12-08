d38cf6ff091a89a7036f145528982f2aab80761e
adrian-str <adrian-str@github.com>
2020-12-08 04:58:48 -0500

    bonus

--- Assignment_4/README.md
+++ Assignment_4/README.md
@@ -1,2 +1,10 @@
 # Assignment 4 script
 ## Run this as: ruby assignment4.rb ./databases/arath.fa ./databases/spombe.fa
+
+## Bonus:
+To confirm the putative orthologues, a phylogenetic tree for each one can be built and analyzed. In order to do this we would need a third species' proteome, ideally related to Arabidopsis and S. pombe. The process would be the following:
+1. Create a blast database of the 3 proteomes.
+2. Run a blast with each putative orthologue as query, either from Arabidopsis or S. pombe.
+3. Get the best hits and extract their fasta sequences.
+4. Make an allignment of these sequences and construct a phylogenetic tree from the allignment.
+5. Infer orthology from speciation events.
--- Assignment_4/assignment4.rb
+++ Assignment_4/assignment4.rb
@@ -4,11 +4,9 @@ arath=ARGV[0]
 spomb=ARGV[1]
 araf=Bio::FlatFile.auto(arath)
 spof=Bio::FlatFile.auto(spomb)
-puts araf.class
-puts spof.class
+
 def db_type(flatfasta)
  entry=Bio::Sequence.auto(flatfasta.next_entry.to_s).guess
- 
   if entry==Bio::Sequence::NA
     type='nucl'
   elsif entry == Bio::Sequence::AA
@@ -20,14 +18,15 @@ end
 def make_db(flatfasta,fasta)
  type=db_type(flatfasta)
  system("makeblastdb -in #{fasta} -dbtype '#{type}' -out $(dirname #{fasta})/$(basename #{fasta} .fa)")
-    
 end
 
 def blast(db,query,type)
  eval='-e 1e-6' #https://doi.org/10.1371/journal.pone.0101850
- factory=Bio::Blast.local(type,"#{File.dirname(db)}/#{File.basename(db,".fa")}",options="#{eval} -F 'm S' -s T") #https://doi.org/10.1093/bioinformatics/btm585
+ factory=Bio::Blast.local("#{type}","#{File.dirname(db)}/#{File.basename(db,".fa")}","-F m S #{eval}" ) #https://doi.org/10.1093/bioinformatics/btm585
  report=factory.query(query)
- return report.hits[0]
+ if report.hits[0]
+  return report.hits[0].definition.split("|")[0].strip
+ end
 end
 
 def blast_type(flat,queries)
@@ -42,18 +41,23 @@ end
 def get_besthit(db,flat,queries)
  besthit=Hash.new
  type=blast_type(flat,queries)
- queries.each_entry do |query|
-  besthit[query.entry_id]=blast(db,query,type)
- end
+ #count=0
+  queries.each_entry do |query|
+   #puts query.entry_id
+   besthit[query.entry_id]=blast(db,query,type)
+   #break if count==20
+   #count+=1
+  end
  return besthit
 end
 
 def get_orthologues(db1,db2,flat1,flat2)
  orthologues=Hash.new
+ puts "Searching for reciprocal best hits..."
  besthits=get_besthit(db1,flat1,flat2)
  flat1.each_entry do |entry|
-  next unless besthits.values.include?(entry.entry_id)
-   reciprocalhits=blast(db2,entry,blast_type(flat2,flat1))
+  next unless besthits.value?(entry.entry_id)
+  reciprocalhits=blast(db2,entry,blast_type(flat2,flat1))
    if besthits[reciprocalhits]==entry.entry_id
     orthologues[reciprocalhits]=entry.entry_id
    end
@@ -63,8 +67,12 @@ end
 
 make_db(spof,spomb)
 make_db(araf,arath)
+#get_besthit(arath,araf,spof)
 orthologues=get_orthologues(arath,spomb,araf,spof)
-puts "Pairs of possible orthologues:"
-orthologues.each do |k,v|
- puts "\t- #{k} and #{v}"
+
+File.open('putative_orthologues.txt', 'w+') do |o|
+ o.puts "Pairs of possible orthologues:"
+ orthologues.each do |k,v|
+  o.puts "\t- #{k} and #{v}"
+ end
 end
